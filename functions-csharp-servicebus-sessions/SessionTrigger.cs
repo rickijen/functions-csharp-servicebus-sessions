151e14110e0e8758a4b22db386d2d1367675851e
Rick Jen <rijen@microsoft.com>
2020-05-10 16:16:16 -0700

    Updated function names

--- functions-csharp-servicebus-sessions/SessionTrigger.cs
+++ functions-csharp-servicebus-sessions/SessionTrigger.cs
@@ -7,14 +7,14 @@ using Microsoft.Extensions.Logging;
 
 namespace Edwards.Function
 {
-    public class SessionTrigger01
+    public class SalesProcessor01
     {
         private readonly IOrderedListClient _client;
-        public SessionTrigger01(IOrderedListClient client) 
+        public SalesProcessor01(IOrderedListClient client) 
         {
             _client = client;
         }
-        [FunctionName("SessionTrigger-01")]
+        [FunctionName("SalesProcessor-01")]
         public async Task Run(
             [ServiceBusTrigger("mq-01", Connection = "integrationtest01_RootManageSharedAccessKey_SERVICEBUS", IsSessionsEnabled = true)]
             Message message,
@@ -23,23 +23,25 @@ namespace Edwards.Function
             string messageId,              
             ILogger log)
         {
+            // Set up additional logging info so we can query the logs in App Insights
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {Encoding.UTF8.GetString(message.Body)}");
             log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
             log.LogInformation($"DeliveryCount={deliveryCount}");
             log.LogInformation($"MessageId={messageId}");
 
+            // Push data into Redis
             await _client.PushData(message.SessionId, Encoding.UTF8.GetString(message.Body));
         }
     }
 
-        public class SessionTrigger02
+        public class SalesProcessor02
     {
         private readonly IOrderedListClient _client;
-        public SessionTrigger02(IOrderedListClient client) 
+        public SalesProcessor02(IOrderedListClient client) 
         {
             _client = client;
         }
-        [FunctionName("SessionTrigger-02")]
+        [FunctionName("SalesProcessor-02")]
         public async Task Run(
             [ServiceBusTrigger("mq-02", Connection = "integrationtest01_RootManageSharedAccessKey_SERVICEBUS", IsSessionsEnabled = true)]
             Message message,
